ed55e321329da268d8eb3884656fcba29187ff88
Chris Maria <git@cjmaria.com>
2022-03-09 22:58:26 -0500

    Unbound config updates

--- one-container/pihole-unbound/build_and_push.sh
+++ one-container/pihole-unbound/build_and_push.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Run this once: docker buildx create --use --name build --node build --driver-opt network=host
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cbcrowe/pihole-unbound:`cat VERSION` --push .
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cbcrowe/pihole-unbound:latest --push .
-
-
+docker buildx build --platform linux/arm64/v8 -t cjmaria/pihole-unbound:latest --push .
